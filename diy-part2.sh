d793a2a829de46492e00e5ea0507d221320f4b42
Schariac2 <2863700045open@gmail.com>
2026-05-03 17:23:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,34 +10,15 @@
 # See /LICENSE for more information.
 #
 
-cat > package/base-files/files/etc/uci-defaults/99-custom-system <<-EOF
-#!/bin/sh
-generate_static_system() {
-	uci -q batch <<-EOF
-		delete system.@system[0]
-		add system system
-		set system.@system[-1].hostname='SchariacWrt'
-		set system.@system[-1].timezone='CST-8'
-		set system.@system[-1].ttylogin='0'
-		set system.@system[-1].log_size='128'
-		set system.@system[-1].urandom_seed='0'
-	EOF
-	uci commit system
-}
-generate_static_system
-EOF
-chmod +x package/base-files/files/etc/uci-defaults/99-custom-system
-
-
-
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/SchariacWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
 
 # Modify banner 
 cat > package/base-files/files/etc/banner << EOF
